17bd66f7588a4b1eb80bec99bf5e8d6df8b87108
Luis <lahoising78@gmail.com>
2023-10-01 14:39:00 -0700

    Basic sets

--- src/lua/sets.lua
+++ src/lua/sets.lua
@@ -1,2 +1,13 @@
 vim.opt.rnu = true
 vim.opt.nu = true
+vim.opt.wrap = false
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.ignorecase = true
+vim.opt.incsearch = true
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.shiftwidth = 2
+
+vim.keymap.set("n", "<Space>", "<Nop>")
+vim.g.mapleader = " "
