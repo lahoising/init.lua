d0354cc009ea8a5e364f460d7a60b99a9689a77e
Luis <lahoising78@gmail.com>
2023-10-15 17:22:31 -0700

    Add LSP for Zig

--- src/lua/language-tools.lua
+++ src/lua/language-tools.lua
@@ -2,24 +2,29 @@ local M = {}
 
 M.tools_by_language = {
 	lua = {
-		lsp = { name = 'lua_ls' },
-		linter = { name = 'luacheck' },
-		formatter = { name = 'stylua' },
+		lsp = { name = "lua_ls" },
+		linter = { name = "luacheck" },
+		formatter = { name = "stylua" },
 	},
 	cpp = {
-		lsp = { name = 'clangd' },
-		linter = { name = 'cpplint' },
-		formatter = { name = 'clangformat', mason_name = 'clang-format' },
+		lsp = { name = "clangd" },
+		linter = { name = "cpplint" },
+		formatter = { name = "clangformat", mason_name = "clang-format" },
 	},
 	java = {
-		lsp = { name = 'jdtls' },
-		linter = { name = 'checkstyle' },
-		formatter = { name = 'clangformat', mason_name = 'clang-format' },
+		lsp = { name = "jdtls" },
+		linter = { name = "checkstyle" },
+		formatter = { name = "clangformat", mason_name = "clang-format" },
 	},
 	rust = {
-		lsp = { name = 'rust_analyzer' },
-		linter = { name = '' },
-		formatter = { name = '' },
+		lsp = { name = "rust_analyzer" },
+		linter = {},
+		formatter = {},
+	},
+	zig = {
+		lsp = { name = "zls" },
+		linter = {},
+		formatter = {},
 	},
 }
 
