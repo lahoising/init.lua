bf60084d11e1b6798319f8a54c0bf6ebd9283d6b
Luis Chiang <lahoising78@gmail.com>
2024-02-09 01:09:03 -0800

    Add LSP support

---
+++ src/lua/plugins_setup/lsp.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+      local lspconfig = require("lspconfig")
+      local mason_lspconfig = require("mason-lspconfig")
+
+      mason_lspconfig.setup_handlers({
+        function(server_name)
+          lspconfig[server_name].setup({})
+        end,
+      })
+    end,
+  },
+}
