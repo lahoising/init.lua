8d312bfbb39f01445ac1b4e112f3942eeac276f0
Luis Chiang <lahoising78@gmail.com>
2024-02-09 01:33:29 -0800

    Add autocompletion

--- src/lua/plugins_setup/lsp.lua
+++ src/lua/plugins_setup/lsp.lua
@@ -4,14 +4,18 @@ return {
     dependencies = {
       "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/nvim-cmp",
     },
     config = function()
       local lspconfig = require("lspconfig")
       local mason_lspconfig = require("mason-lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
       mason_lspconfig.setup_handlers({
         function(server_name)
-          lspconfig[server_name].setup({})
+          lspconfig[server_name].setup({
+            capabilities = capabilities,
+          })
         end,
       })
     end,
