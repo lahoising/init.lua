ddb010424697d4c6b07f89f8c3428373167b5d45
Luis Chiang <lahoising78@gmail.com>
2025-01-11 00:32:13 -0800

    Install theme + Set options

---
+++ src/lua/plugins_setup/theme.lua
@@ -0,0 +1,12 @@
+local default_theme = "onenord"
+
+return {
+	{
+		"rmehri01/onenord.nvim",
+		tag = "v0.7.0",
+		dependencies = {
+      "nvim-tree/nvim-web-devicons"
+    },
+    config = true,
+	},
+}
