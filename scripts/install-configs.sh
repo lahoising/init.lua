4f5260fd1ec8bbeb68ccde8e0c587b1358c2dcfc
Luis Chiang <lahoising78@gmail.com>
2025-01-11 00:25:16 -0800

    Add scripts to install dependencies and neovim

---
+++ scripts/install-configs.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+CONFIG_HOME=~/.config/nvim
+
+mkdir -p $CONFIG_HOME
+
+ln -fs $(pwd)/src/init.lua $CONFIG_HOME/init.lua
+ln -fs $(pwd)/src/lua $CONFIG_HOME/lua
--- scripts/install-neovim.sh
+++ scripts/install-neovim.sh
@@ -1,17 +1,26 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 DOWNLOAD_DIR=/tmp/nvim
 DOWNLOAD_LOCATION=$DOWNLOAD_DIR/nvim.tar.gz
-INSTALL_LOCATION=~/bin/nvim
+INSTALL_DIR=~/bin
+INSTALL_LOCATION=$INSTALL_DIR/nvim
 
 yes | rm -rf $DOWNLOAD_DIR
 mkdir -p $DOWNLOAD_DIR
 
-curl -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz -o $DOWNLOAD_LOCATION
+OS_NAME=$(uname)
+if [[ $OS_NAME == 'Darwin' ]]; then
+  ARCHIVE_NAME=nvim-macos-arm64.tar.gz
+else
+  ARCHIVE_NAME=nvim-linux64.tar.gz
+fi
+
+curl -L https://github.com/neovim/neovim/releases/latest/download/$ARCHIVE_NAME -o $DOWNLOAD_LOCATION
 pushd $DOWNLOAD_DIR
 tar -xzvf $DOWNLOAD_LOCATION
 yes | rm -rf $INSTALL_LOCATION
-mv nvim-linux64 $INSTALL_LOCATION
+mkdir -p $INSTALL_DIR
+mv $(basename $ARCHIVE_NAME .tar.gz) $INSTALL_LOCATION
 popd
 
-sudo ln -f -T $INSTALL_LOCATION/bin/nvim /usr/local/bin/nvim
+sudo ln -fs $INSTALL_LOCATION/bin/nvim /usr/local/bin/nvim
