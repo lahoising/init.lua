7e10cfbdef96457d6443be892bdb7271f4fa9fe4
Luis Chiang <lahoising78@gmail.com>
2025-01-11 13:28:40 -0800

    Set up Mason installed LSPs

--- src/lua/dependencies.lua
+++ src/lua/dependencies.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+M.cmp = "hrsh7th/nvim-cmp"
+M.cmp_nvim_lsp = "hrsh7th/cmp-nvim-lsp"
 M.icons = "icons"
 M.tree_sitter = "tree-sitter"
 M.telescope_fzf_native = "telescope-fzf-native"
---
+++ src/lua/plugins_setup/lsp.lua
@@ -0,0 +1,49 @@
+local deps = require("dependencies")
+
+local M = {}
+
+function M.config()
+  M.require()
+  M.setup_constants()
+  M.setup_handlers()
+end
+
+function M.require()
+  M.lspconfig = require("lspconfig")
+  M.mason_lspconfig = require("mason-lspconfig")
+  M.cmp_nvim_lsp = require("cmp_nvim_lsp")
+end
+
+function M.setup_constants()
+  M.capabilities = M.cmp_nvim_lsp.default_capabilities()
+end
+
+function M.setup_handlers()
+  M.mason_lspconfig.setup_handlers({
+    M.default_handler,
+    ["jdtls"] = M.jdtls_handler,
+  })
+end
+
+function M.default_handler(server_name)
+  M.lspconfig[server_name].setup({
+    capabilities = M.capabilities,
+  })
+end
+
+function M.jdtls_handler()
+  -- TODO: setup jdtls with nvim-jdtls
+end
+
+return {
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      deps.mason,
+      deps.mason_lspconfig,
+      deps.cmp,
+      deps.cmp_nvim_lsp
+    },
+    config = M.config,
+  },
+}
