767caf03f7e8361750320ba7089cbe08fac8ae8e
Luis Chiang <lahoising78@gmail.com>
2025-01-12 17:43:37 -0800

    Format modifications on save

--- src/lua/plugins_setup/formatter.lua
+++ src/lua/plugins_setup/formatter.lua
@@ -1,5 +1,33 @@
 local deps = require("dependencies")
 
+local M = {}
+
+function M.config()
+  M.require()
+  M.setup_formatters()
+  M.setup_autocmds()
+end
+
+function M.require()
+  M.conform = require("conform")
+  M.format_modifications = require("format-modifications").format_modifications
+end
+
+function M.setup_formatters()
+  M.conform.setup({
+    formatters_by_ft = {},
+    default_format_opts = {
+      lsp_format = "fallback",
+    },
+  })
+end
+
+function M.setup_autocmds()
+  vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+    callback = M.format_modifications,
+  })
+end
+
 return {
   {
     "stevearc/conform.nvim",
@@ -7,11 +35,6 @@ return {
       deps.lspconfig,
       deps.mason,
     },
-    opts = {
-      formatters_by_ft = {},
-      default_format_opts = {
-        lsp_format = "fallback",
-      },
-    },
+    config = M.config,
   },
 }
