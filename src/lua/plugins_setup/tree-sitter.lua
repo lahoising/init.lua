170b714f14c2b075b7ef941650f436ed969f994b
Luis Chiang <lahoising78@gmail.com>
2025-02-05 22:37:24 -0800

    Properly set up treesitter

--- src/lua/plugins_setup/tree-sitter.lua
+++ src/lua/plugins_setup/tree-sitter.lua
@@ -5,6 +5,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     name = deps.tree_sitter,
     build = ":TSUpdate",
+    main = "nvim-treesitter.configs",
     opts = {
       auto_install = true,
       highlight = {
